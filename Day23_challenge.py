9a0806588b69e7551f1dd41bbce2407c9cbc9c42
Annie <aniohanian@gmail.com>
2024-02-02 15:51:01 -0800

    Check if a key exists in a dictionary

--- Day23_challenge.py
+++ Day23_challenge.py
@@ -1,9 +1,7 @@
 # Write a program that checks if a key exists in a dictionary.
 def key_exists(dic_list, key):
-    
+
     if key in dic_list:
         return dic_list[key]
             
-        
-
 print(key_exists({"num1": 1,"num2": 2,"num3": 3},"num1"))
