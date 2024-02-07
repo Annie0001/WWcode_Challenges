f0ac94299b58c86cc37b89124ff64b07d5848d1a
Annie <aniohanian@gmail.com>
2024-02-07 01:28:13 -0800

    sorting a list of strings alphabetically

---
+++ Day27_challenge.py
@@ -0,0 +1,8 @@
+# Create a program that sorts a list of strings alphabetically.
+
+def alphabetic_list_sort(my_list):
+    # for item in my_list:
+    return sorted(my_list)
+
+print(alphabetic_list_sort(["Apple","Cold","Bag","Door"]))
+
