826ec1ce47234fd2b47085af0f57c045e1446725
Annie <aniohanian@gmail.com>
2024-02-12 01:02:14 -0800

    Added the base condition for calculating the average of a list of numbers

--- Day32_challenge.py
+++ Day32_challenge.py
@@ -1,6 +1,10 @@
 # Create a function that calculates the average of a list of numbers
 
 def avg_of_list_of_numbers(my_list):
+
+    if len(my_list) == 0:
+        return 0
+    
     sum = 0 
     avg = 0
     for i in range(len(my_list)):
@@ -9,3 +13,4 @@ def avg_of_list_of_numbers(my_list):
     return avg
 
 print(avg_of_list_of_numbers([2,4,6]))
+print(avg_of_list_of_numbers([]))
