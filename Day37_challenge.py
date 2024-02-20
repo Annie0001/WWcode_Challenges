01e11b09ac2ab67c3ff355e06abede0ac5b42ccd
Annie <aniohanian@gmail.com>
2024-02-20 00:33:02 -0800

    Iterate through a dictionary and print its keys and values

--- Day37_challenge.py
+++ Day37_challenge.py
@@ -1,1 +1,7 @@
 # Write a program to iterate through a dictionary and print its keys and values
+
+def key_and_value(dict):
+    for key, value in dict.items():
+        print (key , value)
+
+print(key_and_value({'a':1, 'b':2 , 'c': 4}))
