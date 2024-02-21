b6c95f943f423f1e04b3dcbd9cd83f0119ea1195
Annie <aniohanian@gmail.com>
2024-02-20 18:01:36 -0800

    Flatten a nested list

--- Day38_challenge.py
+++ Day38_challenge.py
@@ -1,9 +1,18 @@
 # Write a program to flatten a nested list
 
-def flatten_list(list_1):
+def flatten_list(my_list):
+    result=[]
+    flatten_list_internal(my_list, result)
+    return result
 
-    for i in list_1:
+def flatten_list_internal(my_list, result):
+    for i in range(len(my_list)):
+        if type(my_list[i]) is list:
+            flatten_list_internal(my_list[i], result)
+        else:
+            result.append(my_list[i])
 
 
 
-print(flatten_list([[1], [2, 3], [4, 5, 6, 7]]))
+#print(flatten_list([[1], [2, 3], [4, 5, 6, 7]]))
+print(flatten_list([[1], [2, 3], [4, 5, 6, 7], [[21,[5,33,11],2]]]))
