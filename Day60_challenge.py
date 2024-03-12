56919ea4879852f5deed3cfd7002b774054da84c
Annie <aniohanian@gmail.com>
2024-03-11 23:49:27 -0700

    bank account with deposit and withdraw

---
+++ Day60_challenge.py
@@ -0,0 +1,15 @@
+# Create a class representing a simple bank account with deposit and withdraw methods
+class bank_account:
+    def __init__(self):
+        self.balance = 0
+
+    def deposit(self, dep_amount):
+        self.balance = self.balance + dep_amount
+    # def withdraw(withdraw_amount):
+
+    def get_balance(self):
+        return self.balance
+
+my_bank_account = bank_account()
+my_bank_account.deposit(10)
+print(my_bank_account.get_balance())
